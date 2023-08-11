ed203cd23946f12272337f57805f29d3f533ae41
CornerSiow <77437729+CornerSiow@users.noreply.github.com>
2023-08-11 09:10:08 +0900

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  4 14:09:31 2023
+
+@author: Corner Siow
+"""
+
+class Node:        
+    def __init__(self, w, index):
+        self.w = w
+        self.neighbourNodes = []
+        self.fromNodes = []
+        self.toNodes = []
+        self.index = index
+    def addNeighbour(self, node):
+        if node not in self.neighbourNodes:
+            self.neighbourNodes.append(node)
+    def addFromNode(self, node):
+        if node not in self.fromNodes:
+            self.fromNodes.append(node)
+    def addToNode(self, node):
+        if node not in self.toNodes:
+            self.toNodes.append(node)            
+    def updateWeight(self, d, learning_rate):
+        self.w += learning_rate * (d - self.w)
+    def updateNeighboardWeight(self, d, learning_rate):
+        for node in self.neighbourNodes:
+            node.updateWeight(d, learning_rate)
