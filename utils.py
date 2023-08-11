213c689a18ea71bd31ca00229e51dc06c171d6d4
CornerSiow <77437729+CornerSiow@users.noreply.github.com>
2023-08-11 12:08:07 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -126,4 +126,93 @@ def getFeatures(data):
     features = np.asarray(features)
     return features
 
+def generateSimilarityFuzzyMembership(node:Node, features, threshold):    
+    A = node.w
+   
+    centerPointResult = []
+    plt.figure()
+    plt.subplot(2, 1, 2)
+    maxDist = 0
+    for n_index, n in enumerate(node.neighbourNodes):
+        B = n.w       
+        if distance.cityblock(B, A) > maxDist:
+            maxDist = distance.cityblock(B, A)
+        
+        dir_AB = (B - A)
+        dir_AB /= np.linalg.norm(dir_AB) 
+
+        # Obtains the data between node A and Node B   
+        temp = []
+        for point in features:
+            if distance.cosine(A, point) >= threshold and distance.cosine(B, point) >= threshold:
+                continue
+            if np.dot(dir_AB, point-A) < 0 or np.dot(-dir_AB, point-B) < 0:
+                continue
+            temp.append(point)
+        temp = np.asarray(temp) 
+        
+        
+        #split to three group
+        group = [[],[],[],[]]     
+        for d in temp:
+            _dist = distance.cosine(A,d)
+            if _dist < (threshold / 4):
+                group[0].append(d)
+            elif _dist < (threshold / 4 * 2):
+                group[1].append(d)
+            elif _dist < (threshold / 4 * 3):
+                group[2].append(d)
+            else: 
+                group[3].append(d)      
+               
+        
+        plt.title("The data between the essential Pose and its neighbour nodes")
+        plt.xlabel("hand position x")
+        plt.ylabel("hand position y")
+        colorList = ["#6060FF","#AAAA00","#00AAAA", "#00FF00"]
+        for i in range(4):
+            group[i] = np.asarray(group[i])
+            plt.scatter(group[i][:,0], group[i][:,1], c=colorList[i], marker='.')
+        
+        if n_index == 0:
+          plt.scatter(A[0], A[1], marker='^', c="#FF0000", label='Essential Pose')   
+          plt.scatter(B[0], B[1], marker='v', c="#FF0000", label='Neighbour Nodes')
+        else:
+          plt.scatter(A[0], A[1], marker='^', c="#FF0000")   
+          plt.scatter(B[0], B[1], marker='v', c="#FF0000")
+        
+        
+        # The closest to Node A
+        m1 = group[0].mean(axis=0)        
+        # The closest to Node B
+        m4 = group[3].mean(axis=0)        
+        # In Transit Between Node A and Node B
+        m2 = group[1].mean(axis=0)
+        m3 = group[2].mean(axis=0)
+        
+        size = 12
+        if n_index == 0:
+          plt.scatter([m1[0],m2[0],m3[0],m4[0]] , [m1[1],m2[1],m3[1],m4[1]] , marker='*', c="#000000", label='Average Point of each group') 
+        else:
+          plt.scatter([m1[0],m2[0],m3[0],m4[0]] , [m1[1],m2[1],m3[1],m4[1]] , marker='*', c="#000000") 
+        plt.annotate("1", (m1[0] , m1[1]), fontsize=size)
+        plt.annotate("2", (m2[0] , m2[1]), fontsize=size)
+        plt.annotate("3", (m3[0] , m3[1]), fontsize=size)
+        plt.annotate("4", (m4[0] , m4[1]), fontsize=size)
+                
+        centerPointResult.append([distance.cityblock(m1, A),distance.cityblock(m2, A),distance.cityblock(m3, A),distance.cityblock(m4, A)])
+    plt.legend()
+    centerPointResult = np.asarray(centerPointResult)    
+  
+    dist1 = max(centerPointResult[:,0])
+    dist2 = max(centerPointResult[:,1])
+    dist3 = max(centerPointResult[:,2])
+    dist4 = max(centerPointResult[:,3])
+    
+    x_similarity = np.arange(0, maxDist, 0.1)
+    similarity_hi = fuzz.trapmf(x_similarity, [0, 0, dist1, dist2])
+    similarity_md = fuzz.trapmf(x_similarity, [dist1, dist2, dist3, dist4])
+    similarity_lo = fuzz.trapmf(x_similarity, [dist3, dist4, maxDist, maxDist])
+    
+    return x_similarity, similarity_hi, similarity_md, similarity_lo
 
